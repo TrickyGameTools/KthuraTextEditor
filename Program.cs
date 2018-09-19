b81c570e918ec96f8e0577e6fd5bfbac9adea2cf
Tricky1975 <facebook@tbbs.nl>
2018-09-19 14:26:00 +0200

    Top menu area

--- Program.cs
+++ Program.cs
@@ -22,10 +22,14 @@
 // 	to the project the exceptions are needed for.
 // Version: 18.09.19
 // EndLic
+
 ﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
 using Gtk;
 using TrickyUnits;
 using TrickyUnits.GTK;
+using UseJCR6;
 
 namespace KthuraTextEditor
 {
@@ -35,22 +39,96 @@ namespace KthuraTextEditor
 
     class MainClass
     {
+        // General config
+        const int base_width = 1600;
+        const int base_height = 1000;
+        const int girl_width = 96;
+        const int girl_height = 96;
+
+        // widgets
+        static MainWindow win;
+        static VBox MainBox;
+        static HBox HeadBox;
+        static HBox WorkBox;
+        static VBox MenuBox;
+        static HBox Menu1;
+        static HBox Menu2;
+        static Image Girl;
+        static Button bNew;
+        static Button bOpen;
+        static Button bClose;
+        static Button bSave;
+        static Button bSaveAll;
+        static Button bQuit;
+
+        // Special widgets
+        static List<Widget> RequiresFile = new List<Widget>(); // Only allow these when a file has actually been loaded and activated.
+
         static void initMKL(){
             MKL.Lic    ("Kthura Text Editor - Program.cs","GNU General Public License 3");
             MKL.Version("Kthura Text Editor - Program.cs","18.09.19");
 
         }
 
-        static void InitGUI(){
+        static void initJCR6(){
+            JCR6_lzma.Init();
+            JCR6_zlib.Init();
+        }
+
+        static void initGirl(){
+            Assembly asm = Assembly.GetExecutingAssembly();
+            System.IO.Stream stream = asm.GetManifestResourceStream("KthuraTextEditor.Properties.Kthura.png");
+            Gdk.Pixbuf PIX = new Gdk.Pixbuf(stream);
+            win.Icon = PIX;
+            stream.Dispose();
+            stream = asm.GetManifestResourceStream("KthuraTextEditor.Properties.Kthura.png");
+            Girl = new Image(stream);
+            Girl.SetSizeRequest(girl_width,girl_height);
+            Girl.SetAlignment(0, 0);
+            stream.Dispose();
+            HeadBox.Add(Girl);
+        }
+
+        static void initGUI(){
             Application.Init();
-            MainWindow win = new MainWindow();
-            win.Show();
+            win = new MainWindow();
+            win.Title = "Kthura Text Editor";
+            win.SetSizeRequest(base_width,base_height);
+            MainBox = new VBox();
+            HeadBox = new HBox(); HeadBox.SetSizeRequest(base_width, girl_width);
+            WorkBox = new HBox(); WorkBox.SetSizeRequest(base_width, base_height - girl_height);
+            MainBox.Add(HeadBox);
+            MainBox.Add(WorkBox);
+            initGirl();
+            MenuBox = new VBox();
+            MenuBox.SetSizeRequest(base_width - girl_width, girl_height);
+            HeadBox.Add(MenuBox);
+            Menu1 = new HBox();
+            Menu2 = new HBox();
+            MenuBox.Add(Menu1);
+            MenuBox.Add(Menu2);
+            bNew = new Button("New");
+            bOpen = new Button("Open");
+            bClose = new Button("Close"); RequiresFile.Add(bClose);
+            bSave = new Button("Save"); RequiresFile.Add(bSave);
+            bSaveAll = new Button("SaveAll");
+            bQuit = new Button("Quit");
+            Menu1.Add(bNew);
+            Menu1.Add(bOpen);
+            Menu1.Add(bClose);
+            Menu2.Add(bSave);
+            Menu2.Add(bSaveAll);
+            Menu2.Add(bQuit);
+            win.Add(MainBox);
+            win.ShowAll();
             Application.Run();
         }
 
         public static void Main(string[] args)
         {
             initMKL();
+            initJCR6();
+            initGUI();
         }
     }
 }
