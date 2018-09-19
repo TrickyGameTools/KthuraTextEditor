b1e96596bd13b6dc19254e3f097afdbad625f5eb
Tricky1975 <facebook@tbbs.nl>
2018-09-19 13:32:25 +0200

    Template This is the intial commit. All I did was make Visual Studio create a template and added the dependency files I expect to need. Cut short nothing usable yet!

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Gtk;
+
+namespace KthuraTextEditor
+{
+    class KthuraLoadedFile{
+
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}
