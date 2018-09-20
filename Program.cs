ea0cdf8867521efa3c6a52818c9a991270fe06da
Tricky1975 <facebook@tbbs.nl>
2018-09-20 18:23:08 +0200

    Most of UI worked out

--- Program.cs
+++ Program.cs
@@ -34,6 +34,14 @@ using UseJCR6;
 namespace KthuraTextEditor
 {
     class KthuraLoadedFile{
+        public string Objects;
+        public string Settings;
+        public SortedDictionary<string, byte[]> Unknown = new SortedDictionary<string, byte[]>();
+        public bool allowzlib;
+        public bool allowlzma;
+        public Dictionary<string, string> GeneralData = new Dictionary<string, string>();
+        public ListStore LsGenData = new ListStore(typeof(string), typeof(string));
+        public Dictionary<object, string> LinkGeneralData = new Dictionary<object, string>();
 
     }
 
@@ -44,6 +52,8 @@ namespace KthuraTextEditor
         const int base_height = 1000;
         const int girl_width = 96;
         const int girl_height = 96;
+
+        readonly static string[] kthuramusthave = "Data;Objects;Settings".Split(';');
 
         // widgets
         static MainWindow win;
@@ -62,15 +72,34 @@ namespace KthuraTextEditor
         static Button bQuit;
         static ListBox OpenFiles;
         static Notebook Tabber;
+        static ScrolledWindow swObjects;
+        //static HBox panObjects;
+        //static VScrollbar sbObjects;
+        static TextView eObjects;
+        static ScrolledWindow swSettings;
+        static TextView eSettings;
+        static ScrolledWindow swMisc;
+        static TreeView vMisc;
 
 
         // Special widgets
         static List<Widget> RequiresFile = new List<Widget>(); // Only allow these when a file has actually been loaded and activated.
 
+        // The actual stuff itself
+        static readonly SortedDictionary<string, KthuraLoadedFile> Loaded = new SortedDictionary<string, KthuraLoadedFile>();
+        static KthuraLoadedFile Current {
+            get {
+                if (OpenFiles.ItemText == "") return null;
+                return Loaded[OpenFiles.ItemText];
+            }
+        }
+
         static void initMKL(){
             MKL.Lic    ("Kthura Text Editor - Program.cs","GNU General Public License 3");
             MKL.Version("Kthura Text Editor - Program.cs","18.09.19");
-
+            QOpen.Hello();
+            QuickGTK.Hello();
+            ListBox.Hello();
         }
 
         static void initJCR6(){
@@ -92,9 +121,98 @@ namespace KthuraTextEditor
             HeadBox.Add(Girl);
         }
 
+        static void AutoEnable(){
+            var able = Current != null;
+            foreach(Widget w in RequiresFile){
+                w.Sensitive = able;
+            }
+        }
+
+        static void UpdateMapList(){
+            OpenFiles.Clear();
+            foreach (string f in Loaded.Keys) OpenFiles.AddItem(f);
+        }
+
+
+        static void Load(string file){
+            var j = JCR6.Dir(file);
+            if (j == null) { QuickGTK.Error("JCR6 failed to analyse this mapfile\n\n" + JCR6.JERROR); return;  }
+            foreach(string musthave in kthuramusthave){
+                if (!j.Exists(musthave)) {
+                    QuickGTK.Error($"The required entry {musthave} does not appear to exist in this JCR6 file.\n\nAre you sure this is a Kthura map file?");
+                    return;
+                }
+            }
+            var lkthura = new KthuraLoadedFile();
+            Loaded[file] = lkthura;
+            foreach (string e in j.Entries.Keys)
+            {
+                var iEntry = j.Entries[e];
+                lkthura.allowlzma = lkthura.allowlzma || iEntry.Storage == "lzma";
+                lkthura.allowzlib = lkthura.allowzlib || iEntry.Storage == "zlib";
+                switch (e)
+                {
+                    case "DATA":
+                        lkthura.GeneralData = j.LoadStringMap("Data");
+                        break;
+                    case "OBJECTS":
+                        lkthura.Objects = j.LoadString("Objects");
+                        break;
+                    case "SETTINGS":
+                        lkthura.Settings = j.LoadString("Settings");
+                        break;
+                    default:
+                        lkthura.Unknown[iEntry.Entry] = j.JCR_B(e);
+                        break;
+
+                }
+            }
+            UpdateMapList();
+        }
+
+        static void OnOpenFile(object sender, EventArgs a){
+            var file = QuickGTK.RequestFile("Please select a Kthura Map File");
+            if (file == "") return;
+            if (Loaded.ContainsKey(file)) {
+                QuickGTK.Warn("That file has already been loaded!");
+                return;
+            }
+            Load(file);
+        }
+
+        static void OnFileSelect(object sender, EventArgs a){
+            AutoEnable();
+            var i = OpenFiles.ItemText;
+            if (i == "") return;
+            // Objects
+            eObjects.Buffer.Text = Current.Objects;
+            // Settings
+            eSettings.Buffer.Text = Current.Settings;
+            // Misc files (files this text editor cannot handle, but which should be loaded and saved anyway).
+            var lsMisc = new ListStore(typeof(string),typeof(string));
+            foreach(string k in Current.Unknown.Keys){
+                var v = Current.Unknown[k];
+                var l = v.Length;
+                var ln = "Empty";
+                if (l > 0)
+                {
+                    if (l == 1) { ln = "One byte"; }
+                    else if ( l < 5000) { ln = $"{l} bytes"; }
+                    else if (l < 5000000) { ln = $"{Math.Round((double)l / 1024)} Kilobytes"; }
+                    else if (l < 50000000) { ln = $"{Math.Round((double)l / (1024 * 1024))} Megabytes"; }
+                    else { ln= $"{Math.Round((double)l / (1024 * 1024 *1024))} Gigabytes"; }
+                }
+                lsMisc.AppendValues(k, ln);
+                vMisc.Model = lsMisc;
+            }
+
+
+        }
+
         static void initGUI(){
             Application.Init();
             win = new MainWindow();
+            QuickGTK.MyMainWindow = win;
             win.Title = "Kthura Text Editor";
             win.SetSizeRequest(base_width,base_height);
             MainBox = new VBox();
@@ -123,18 +241,47 @@ namespace KthuraTextEditor
             Menu2.Add(bSaveAll);
             Menu2.Add(bQuit);
             bGit.Clicked += delegate (object sender, EventArgs a) { OURI.OpenUri("https://github.com/TrickyGameTools/KthuraTextEditor"); };
-            OpenFiles = new ListBox();
+            bOpen.Clicked += OnOpenFile;
+            OpenFiles = new ListBox("Loaded Kthura Maps");
             OpenFiles.AddTo(WorkBox);
             OpenFiles.SetSizeRequest(250, base_height - girl_height);
+            OpenFiles.Gadget.CursorChanged += OnFileSelect;
             Tabber = new Notebook();
             Tabber.SetSizeRequest(base_width - 250, base_height - girl_height);
             var about = new Label("Kthura Text Editor\n\nCoded by: Tricky\n(c) Jeroen P. Broks\n\nReleased under the terms of the GPL 3\n\n" + MKL.All());
             about.SetAlignment(0, 0);
+            eObjects = new TextView();
+            swObjects = new ScrolledWindow();
+            swObjects.SetSizeRequest(base_width - 250, base_height - girl_height);
+            swObjects.Add(eObjects);
+            RequiresFile.Add(eObjects);
+            eSettings = new TextView();
+            swSettings = new ScrolledWindow();
+            swSettings.SetSizeRequest(base_width - 250, base_height - girl_height);
+            swSettings.Add(eSettings);
+            RequiresFile.Add(eSettings);
+            swMisc = new ScrolledWindow();
+            vMisc = new TreeView();
+            var ncMisc = new CellRendererText();
+            var tcMisc = new TreeViewColumn();
+            tcMisc.Title = "Unknown Entry:";
+            tcMisc.PackStart(ncMisc, true);
+            tcMisc.AddAttribute(ncMisc, "text", 0);
+            vMisc.AppendColumn(tcMisc);
+            tcMisc = new TreeViewColumn();
+            tcMisc.Title = "Size:";
+            tcMisc.PackStart(ncMisc, true);
+            tcMisc.AddAttribute(ncMisc, "text", 1);
+            vMisc.AppendColumn(tcMisc);
+            swMisc.Add(vMisc);
+            RequiresFile.Add(vMisc);
             Tabber.AppendPage(about, new Label("About"));
             Tabber.AppendPage(new Label("Coming Soon"), new Label("General Data"));
-            Tabber.AppendPage(new Label("Coming Soon"), new Label("Objects"));
-            Tabber.AppendPage(new Label("Coming Soon"), new Label("Misc"));
+            Tabber.AppendPage(swObjects, new Label("Objects"));
+            Tabber.AppendPage(swSettings, new Label("Settings"));
+            Tabber.AppendPage(swMisc, new Label("Misc"));
             WorkBox.Add(Tabber);
+            AutoEnable();
             win.Add(MainBox);
             win.ShowAll();
             Application.Run();
